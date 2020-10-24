389e6ac4d050deb5416ec94dffa47f7add214840
Jonah Keller <jkeller10@stuy.edu>
2020-10-23 21:10:34 -0400

    "Callable by Tester.java"

---
+++ ArrayOps.java
@@ -0,0 +1,33 @@
+public class ArrayOps{
+
+  public static int sum(int[] arr)
+  {
+    int Count_Sum = 0;
+    for (int i = 0 ; i < arr.length; i++)
+    {
+      Count_Sum += arr[i];
+    }
+    return Count_Sum;
+    // Takes an arry of int and returns a sum of
+    //the ind. values in the ArrayOps
+  }
+
+  public static int largest(int[] arr)
+  {
+    int A = 4;
+    return A;
+  }
+
+  public static int[] sumRows(int[][] matrix)
+  {
+    int[]B = {5,4};
+    return B;
+  }
+
+  public static int[] largestInRows (int[][] matrix)
+  {
+    int[]C = {5,4} ;
+    return C;
+
+  }
+}
---
+++ Tester.java
@@ -0,0 +1,7 @@
+public class Tester{
+  public static void main(String[] args)
+  { int[]A = {1, 2, 3};
+    System.out.println(ArrayOps.sum(A));
+  }
+
+}
