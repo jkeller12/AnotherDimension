7c0fe00a4da147579488a60926913cfe3095bd23
Jonah Keller <jkeller10@stuy.edu>
2020-10-23 21:33:02 -0400

    Sum rows now functions

--- Tester.java
+++ Tester.java
@@ -1,7 +1,10 @@
 public class Tester{
   public static void main(String[] args)
-  { int[]A = {1, 2, 3};
-    System.out.println(ArrayOps.largest(A));
+  { int[][]A = {{1,2,3},{1,2,3}};
+
+    System.out.println(ArrayOps.sumRows(A));
+    System.out.println(Demo.arrToString(ArrayOps.sumRows(A)));
+    
   }
 
 }
