21405d6e2c583fa260be493d69b0d854a8a9686d
Jonah Keller <jkeller10@stuy.edu>
2020-10-23 21:36:11 -0400

    The Tester compiles w/ Largest in Rows

--- Tester.java
+++ Tester.java
@@ -2,9 +2,8 @@ public class Tester{
   public static void main(String[] args)
   { int[][]A = {{1,2,3},{1,2,3}};
 
-    System.out.println(ArrayOps.sumRows(A));
-    System.out.println(Demo.arrToString(ArrayOps.sumRows(A)));
-    
+    System.out.println(Demo.arrToString(ArrayOps.largestInRows(A)));
+
   }
 
 }
