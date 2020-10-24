20c9bd795dd5ecb28d4a47b6cdf8f759bc17cd69
Jonah Keller <jkeller10@stuy.edu>
2020-10-23 22:09:29 -0400

    isColMagic runs properly and tester compiles and runs

--- Tester.java
+++ Tester.java
@@ -2,10 +2,10 @@ public class Tester{
   public static void main(String[] args)
   { int[][]  A = { {  1,  2, 3, 4 },
    {  2, 3,  4,  1 },
-   { 3, 4,  1, 2 } }; 
+   { 3, 4,  1, 2 } };
     int[]  B  =   {  1, 3, 5 };
 
-    System.out.println((ArrayOps.isRowMagic(A)));
+    System.out.println((ArrayOps.isColMagic(A)));
 
   }
 
